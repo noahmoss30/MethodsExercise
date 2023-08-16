a8504869595f745c06a28c7e0713fd7ac05a6900
Noah Moss <noahmoss30@gmail.com>
2023-08-15 22:49:31 -0500

    Began user questions

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //----------------------Exercise 1----------------------
+            //Name: Noah
+            //Favorite Color: Blue
+            //Favorite Animal: Walrus
+            //Favorite Band: The Beatles
+
+
+
+            Console.WriteLine($"Hello- What is your first name?");
+           var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {userName}, What is your favorite color?");
+           var color = Console.ReadLine();
+
+            Console.WriteLine($"{color} is an awesome color! What is your favorite animal?");
+           var animal = Console.ReadLine();
+
+            Console.WriteLine($"Great! Now, what is your favorite band?");
+           var band = Console.ReadLine();
+
         }
     }
 }
