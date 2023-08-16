2eae01d126349db710f110236872f346be13c2ae
Noah Moss <noahmoss30@gmail.com>
2023-08-15 23:00:12 -0500

    Finished Profile

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -24,6 +24,15 @@
             Console.WriteLine($"Great! Now, what is your favorite band?");
            var band = Console.ReadLine();
 
+            Console.WriteLine($"Thanks, {userName}!  Here is your profile");
+            Console.WriteLine("--------------------------------");
+          
+
+            Console.WriteLine($"Name: {userName}");
+            Console.WriteLine($"Favorite Color: {color}");
+            Console.WriteLine($"Favorite Animal: {animal}");
+            Console.WriteLine($"Favorite Band: {band}");
+
         }
     }
 }
